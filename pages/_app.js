e8dcda8a8a63a04d1e9b8af50e2abf061daf0617
Theo Iyonor <symplytheo@gmail.com>
2020-11-26 19:05:46 +0100

    Fix persistent cart

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,33 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
 import { CartProvider } from '../src/CartContext'
+import { useDispatchCart } from '../src/CartContext'
+
+const Hydrate = () => {
+  const dispatch = useDispatchCart()
+  
+  React.useEffect(() => {
+   if (typeof window !== "undefined") {
+     const CartLS = JSON.parse(localStorage.getItem('cart')) || []
+     const getCart = (item) => dispatch({ type: 'SET_CART', payload: item })
+     getCart(CartLS)
+   }
+  }, [])
+
+  return <></>
+}
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-
+  
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
+    
+    
   }, []);
 
   return (
@@ -27,6 +44,7 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <CartProvider>
+          <Hydrate />
           <Component {...pageProps} />
         </CartProvider>
       </ThemeProvider>
